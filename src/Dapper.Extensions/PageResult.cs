d969f9aa9910964634f9e3364f913f93981d08b6
Owen <315872858@qq.com>
2019-10-11 21:49:12 +0800

    Update PageResult.cs

--- src/Dapper.Extensions/PageResult.cs
+++ src/Dapper.Extensions/PageResult.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Dapper.Extensions
 {
@@ -6,6 +7,7 @@ namespace Dapper.Extensions
     /// 分页
     /// </summary>
     /// <typeparam name="T"></typeparam>
+    [Serializable]
     public class PageResult<T>
     {
         /// <summary>
