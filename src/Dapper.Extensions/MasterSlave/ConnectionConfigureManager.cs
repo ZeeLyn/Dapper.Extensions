51acb65852f953724e4c7b401edcf7f8eff3f285
Owen <315872858@qq.com>
2019-10-23 16:39:44 +0800

    V3.0.0
    1.Added support for console application.
    2.Added sql separate.
    3.Added support for read and write separation.

--- src/Dapper.Extensions/MasterSlave/ConnectionConfigureManager.cs
+++ src/Dapper.Extensions/MasterSlave/ConnectionConfigureManager.cs
@@ -1,5 +1,9 @@
 ﻿using System.Collections.Concurrent;
 using Microsoft.Extensions.Configuration;
+using System;
+using System.Linq;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Primitives;
 
 namespace Dapper.Extensions.MasterSlave
 {
@@ -9,12 +13,15 @@ namespace Dapper.Extensions.MasterSlave
 
         private ILoadBalancing LoadBalancing { get; }
 
+        private ILogger Logger { get; }
+
         private readonly ConcurrentDictionary<string, ConnectionConfiguration> _connections =
             new ConcurrentDictionary<string, ConnectionConfiguration>();
-        public ConnectionConfigureManager(IConfiguration configuration, ILoadBalancing loadBalancing)
+        public ConnectionConfigureManager(IConfiguration configuration, ILoadBalancing loadBalancing, ILogger<ConnectionConfigureManager> logger)
         {
             Configuration = configuration;
             LoadBalancing = loadBalancing;
+            Logger = logger;
         }
 
         public string GetConnectionString(string connectionName, bool readOnly = false)
@@ -22,17 +29,36 @@ namespace Dapper.Extensions.MasterSlave
             var connection = _connections.GetOrAdd(connectionName, name =>
             {
                 Configure(name);
-                return Configuration.GetSection($"ConnectionStrings:{connectionName}").Get<ConnectionConfiguration>();
+                return Bind(name);
             });
             return readOnly ? LoadBalancing.NextConnectionString(connection.Slaves) : connection.Master;
         }
 
+        private ConnectionConfiguration Bind(string connectionName)
+        {
+            var section = Configuration.GetSection($"ConnectionStrings:{connectionName}");
+            if (!section.Exists())
+            {
+                Logger.LogError($"Configuration node 'ConnectionStrings:{connectionName}' not found.");
+                throw new Exception($"Configuration node 'ConnectionStrings:{connectionName}' not found.");
+            }
+            var configure = section.Get<ConnectionConfiguration>();
+            if (configure == null || string.IsNullOrWhiteSpace(configure.Master) || configure.Slaves == null || !configure.Slaves.Any())
+            {
+                Logger.LogError($"Configuration node 'ConnectionStrings:{connectionName}' error.");
+                throw new Exception($"Configuration node 'ConnectionStrings:{connectionName}' error.");
+            }
+            return configure;
+        }
+
         private void Configure(string connectionName)
         {
-            Configuration.GetSection($"ConnectionStrings:{connectionName}").GetReloadToken().RegisterChangeCallback(state =>
-                {
-                    _connections[connectionName] = Configuration.GetSection($"ConnectionStrings:{connectionName}").Get<ConnectionConfiguration>();
-                }, connectionName);
+            var section = Configuration.GetSection($"ConnectionStrings:{connectionName}");
+            ChangeToken.OnChange<string>(() => section.GetReloadToken(), name =>
+            {
+                _connections[name.ToString()] = Bind(name.ToString());
+            }, connectionName);
+
         }
     }
 }
