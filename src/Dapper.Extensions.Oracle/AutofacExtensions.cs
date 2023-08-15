bcdc4768f02c064e034287fdb284cba5282ea6f7
Owen <zeelyn@qq.com>
2023-08-15 18:56:27 +0800

    Support for Oracle data

---
+++ src/Dapper.Extensions.Oracle/AutofacExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Autofac;
+using Dapper.Extensions.Monitor;
+
+namespace Dapper.Extensions.Oracle
+{
+    public static class AutofacExtensions
+    {
+        public static ContainerBuilder AddDapperForODBC(this ContainerBuilder container,
+            string connectionName = "DefaultConnection", string serviceKey = null, bool enableMasterSlave = false,
+            Action<MonitorBuilder> monitorBuilder = null)
+        {
+            container.AddDapper<OracleDapper>(connectionName, serviceKey, enableMasterSlave, monitorBuilder);
+            return container;
+        }
+
+        public static ContainerBuilder AddDapperForODBC(this ContainerBuilder container, Action<DapperBuilder> builder,
+            Action<MonitorBuilder> monitorBuilder = null)
+        {
+            var configure = new DapperBuilder();
+            builder?.Invoke(configure);
+            container.AddDapper<OracleDapper>(configure.ConnectionName, configure.ServiceKey,
+                configure.EnableMasterSlave,
+                monitorBuilder);
+            return container;
+        }
+    }
+}
