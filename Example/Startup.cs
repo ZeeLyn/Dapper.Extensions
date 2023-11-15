7e0be9f5114ace649b7fbde065d9058504b34ab0
Owen <zeelyn@qq.com>
2023-11-15 13:06:18 +0800

    Update Startup.cs

--- Example/Startup.cs
+++ Example/Startup.cs
@@ -68,11 +68,11 @@ namespace Example
 
             #region Enable Caching
 
-            //services.AddDapperCachingInRedis(new RedisConfiguration
-            //{
-            //    AllMethodsEnableCache = false,
-            //    ConnectionString = "localhost:6379,password=nihao123"
-            //});
+            services.AddDapperCachingInRedis(new RedisConfiguration
+            {
+                AllMethodsEnableCache = false,
+                ConnectionString = "localhost:6379,password=nihao123"
+            });
 
             //services.AddDapperCachingInPartitionRedis(new PartitionRedisConfiguration
             //{
